2c9143fd4e9edf9180402cbbdc521a79bb053524
Roma <romanlychak@ukr.net>
2016-06-17 19:41:21 +0300

    include config class IoTableConfig

--- src/main/java/org/rldev/config/AppConfig.java
+++ src/main/java/org/rldev/config/AppConfig.java
@@ -1,6 +1,11 @@
 package org.rldev.config;
 
 
+import org.rldev.iotable.model.ioUnits.AnalogInput;
+import org.rldev.iotable.model.ioUnits.AnalogOutput;
+import org.rldev.iotable.model.ioUnits.DigitalInput;
+import org.rldev.iotable.model.ioUnits.DigitalOutput;
+import org.rldev.iotable.normalize.validation.*;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -13,7 +18,7 @@ import org.springframework.context.annotation.Import;
 import java.io.File;
 
 @SpringBootApplication(scanBasePackages = "org.rldev.*", exclude = { JacksonAutoConfiguration.class, SecurityAutoConfiguration.class})
-@Import({WebConfig.class})
+@Import({WebConfig.class, IoTableConfig.class})
 public class AppConfig extends SpringBootServletInitializer {
 
     public static final String IOTABLES_DIRECTORY = "uploadIoTables";
