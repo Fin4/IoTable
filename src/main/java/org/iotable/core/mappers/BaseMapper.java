7cce9045a092f1e35ced53bd284177fe0c17b206
Fin4 <romanlychak@ukr.net>
2016-08-15 20:42:37 +0300

    make class abstract

--- src/main/java/org/iotable/core/mappers/BaseMapper.java
+++ src/main/java/org/iotable/core/mappers/BaseMapper.java
@@ -4,7 +4,7 @@ import org.iotable.core.Config;
 import org.iotable.core.model.ioUnits.IoUnit;
 import org.iotable.core.mappers.exceptions.TemplateStringException;
 
-public class BaseMapper {
+public abstract class BaseMapper {
 
     public String generateCode(IoUnit ioUnit, String template) throws TemplateStringException {
 
