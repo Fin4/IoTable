66caa279ce08787c76862084d9ee1d4fb599e520
Fin4 <romanlychak@ukr.net>
2016-08-15 20:44:00 +0300

    rename to BaseValidator

--- src/main/java/org/iotable/core/normalize/validation/BaseValidator.java
+++ src/main/java/org/iotable/core/normalize/validation/BaseValidator.java
@@ -3,7 +3,7 @@ package org.iotable.core.normalize.validation;
 
 import org.iotable.core.model.ioUnits.IoUnit;
 
-public abstract class BaseIoUnitValidator {
+public abstract class BaseValidator {
 
     private static final char cyrillicChars[] = {'А', 'В', 'Е', 'К', 'М', 'Н', 'О', 'Р', 'С', 'Т', 'Х', 'І', 'У'};
     private static final char latinChars[] = {'A', 'B', 'E', 'K', 'M', 'H', 'O', 'P', 'C', 'T', 'X', 'I', 'Y'};
