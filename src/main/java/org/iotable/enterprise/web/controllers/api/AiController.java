218519aa1f3555640cc6b980d49a0ef30716f422
Fin4 <romanlychak@ukr.net>
2016-08-19 21:11:10 +0300

    init

--- src/main/java/org/iotable/enterprise/web/controllers/api/AiController.java
+++ src/main/java/org/iotable/enterprise/web/controllers/api/AiController.java
@@ -1,9 +1,14 @@
 package org.iotable.enterprise.web.controllers.api;
 
+import org.iotable.core.mappers.AiMapper;
+import org.iotable.core.mappers.exceptions.TemplateStringException;
 import org.iotable.core.model.IoTable;
 import org.iotable.core.model.ioUnits.AnalogInput;
+import org.iotable.enterprise.web.controllers.api.requestWrappers.AiMapWrapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -11,8 +16,23 @@ import java.util.List;
 @SessionAttributes("iotable")
 public class AiController {
 
+    @Autowired
+    private AiMapper aiMapper;
+
     @RequestMapping(value = "/aiTable", method = RequestMethod.GET)
     public List<AnalogInput> getAnalogInputs(@ModelAttribute("iotable") IoTable ioTable) {
         return ioTable.getAnalogInputs();
+    }
+
+    @RequestMapping(value = "/aiTable/mapper", method = RequestMethod.POST)
+    public List<String> mapAnalogInputs(@RequestBody AiMapWrapper aiMapWrapper) {
+
+        List<String> strings;
+        try {
+            strings = aiMapper.generateCode(aiMapWrapper.getAnalogInputs(), aiMapWrapper.getTemplate());
+        } catch (TemplateStringException e) {
+            strings = new ArrayList<>();
+        }
+        return strings;
     }
 }
